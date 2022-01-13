f10083f152f57f21c12cc576965463f20f9650b8
TEX910 <daniele.tesi95@gmail.com>
2022-01-13 08:44:13 +0100

    Firebase + Google Auth - try1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+
+const config = {
+  apiKey: "AIzaSyDQejVt0uPATnMFWFb6CKnD75WvPdGvhLk",
+  authDomain: "webibook-webiapp.firebaseapp.com",
+  projectId: "webibook-webiapp",
+  storageBucket: "webibook-webiapp.appspot.com",
+  messagingSenderId: "1093825676681",
+  appId: "1:1093825676681:web:d5598e702534f35fb00d58",
+  measurementId: "G-PMG64Y9DKG"
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
